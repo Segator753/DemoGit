dc70b8fc7f42bca15b1fabb07e5664345eecb940
Segator753 <140981020+Segator753@users.noreply.github.com>
2023-08-26 05:35:11 -0500

    Initial commit

---
+++ src/main/java/com/bruno/demogit/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.bruno.demogit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String saludar() {
+        return "index";
+    }
+ }
